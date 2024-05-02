8519e02b33e3ae2651df38a5704950fd12051ac4
witoAPCS <143124114+witoAPCS@users.noreply.github.com>
2024-05-01 18:46:20 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -8,6 +8,11 @@ public class CheckDigit
    public static int getCheck(int num) 
    {  
      /* to be implemented in part (a) */
+          int sum = 0;
+     for (int i = 1; i <= getNumberOfDigits(num); i++){
+       sum += (8 - i) * getDigit(num,i);
+     }
+     return sum % 10; 
    }
  
   /** Returns true if numWithCheckDigit is valid, or false    
@@ -19,6 +24,16 @@ public class CheckDigit
    public static boolean isValid(int numWithCheckDigit)    
    {      
      /* to be implemented in part (b) */    
+      int check = numWithCheckDigit % 10;
+      int num = numWithCheckDigit /10; 
+      int newCheck = getCheck(num); 
+     if (check == newCheck)
+     {
+       return true;
+     }
+     else{
+     return false;
+     }
    }    
    
    /** Returns the number of digits in num. */    
